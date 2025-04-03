6ad0257b9864831e3867f109c980fed5062438cf
bybeejac000 <130931384+bybeejac000@users.noreply.github.com>
2025-04-03 17:27:14 -0600

    Get content filtering

--- src/Recommendation.tsx
+++ src/Recommendation.tsx
@@ -13,6 +13,12 @@ const Recommendation: React.FC = () => {
     //content: [],
     //azure: [],
   });
+  const [content, setContent] = useState<Recommendations>({
+    collaborative: [],
+    //content: [],
+    //azure: [],
+  });
+  const [itemId, setItemId] = useState<string>("");
 
   const fetchRecommendations = async () => {
     try {
@@ -35,34 +41,84 @@ const Recommendation: React.FC = () => {
     }
   };
 
+  const fetchContent = async () => {
+    try {
+      const response = await fetch(
+        `http://172.200.211.84:5000/get_recommendations?title=${itemId}`
+      );
+      const jsonData = await response.json(); // Parse the JSON response once
+      console.log(jsonData.recommendations);
+      if (jsonData.recommendations) {
+        setContent({
+          collaborative: Array.isArray(jsonData.recommendations)
+            ? jsonData.recommendations
+            : [],
+          //content: contentData,
+          //azure: azureData,
+        });
+      }
+    } catch (error) {
+      console.error("Error fetching recommendations:", error);
+    }
+  };
+
   return (
     <div>
+      <label htmlFor="UserId">UserID </label>
       <input
+        id="UserId"
         type="text"
         placeholder="Enter UserID"
         value={selectedID}
         onChange={(e) => setSelectedID(e.target.value)}
         style={{ marginRight: "20px" }}
       />
-      <button onClick={fetchRecommendations}>Get Recommendations</button>
+      <button
+        onClick={() => {
+          fetchRecommendations();
+        }}
+      >
+        Get Recommendations
+      </button>
+      <br />
+      <div>
+        <label htmlFor="content">ItemId </label>
+        <input
+          id="content"
+          type="text"
+          placeholder="Enter ItemId"
+          value={itemId}
+          onChange={(e) => setItemId(e.target.value)}
+          style={{ marginRight: "20px" }}
+        />
+        <button
+          onClick={() => {
+            fetchContent();
+          }}
+        >
+          Get Recommendations
+        </button>
+        <p>Collaborative Filtering</p>
+        <ul>
+          {recommendations.collaborative.map((id, index) => (
+            <li key={index}>{id}</li>
+          ))}
+        </ul>
 
-        <br />
-        <br />
-
-      <p><b>Collaborative Filtering</b></p>
-      <ul style={{ textAlign: "left" }}>
-        {recommendations.collaborative.length > 0 ? (
-          recommendations.collaborative.map((item, index) => (
-            <li key={index}>{item}</li>
-          ))
-        ) : (
-          <p>No recommendations available.</p>
-        )}
-      </ul>
-
-      <p><b>Content Filtering Goes Here</b></p>
-
-      <p><b>Azure Goes Here</b></p>
+        <p>Content Filtering</p>
+        <ul>
+          {content.collaborative.map((id, index) => (
+            <li key={index}>{id}</li>
+          ))}
+        </ul>
+        {/*}
+        <p>Azure ML</p>
+        <ul>
+          {recommendations.azure.map((id, index) => (
+            <li key={index}>{id}</li>
+          ))}
+        </ul> */}
+      </div>
     </div>
   );
 };
